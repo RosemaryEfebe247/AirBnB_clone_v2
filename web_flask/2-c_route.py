b65650049183c781fe744fc168c25eafbb89031a
root <root@DESKTOP-R3J48K6>
2023-07-22 20:01:50 +0100

    Web_flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -25,7 +25,7 @@ def hbnb():
 def c(text):
     """Display content of text"""
     text = text.replace("_", " ")
-    return "C {}".format(text)
+    return "C $text"
 
 
 if __name__ == '__main__':
