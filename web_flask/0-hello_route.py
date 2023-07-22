9d95036b965cfe213c6012098adc689f31e8e31f
root <root@DESKTOP-R3J48K6>
2023-07-22 18:42:23 +0100

    Web_flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,15 +2,15 @@ from flask import Flask
 """A script that starts flask web application"""
 
 
-app = Flask(__name__, strict_slashes=False)
+app = Flask(__name__)
 """Sets strict slash verification to false"""
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def home():
     """
     Describes the home page message
     Return: greeting
     """
     return "Hello HBNB!"
-if __name__ = '__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
