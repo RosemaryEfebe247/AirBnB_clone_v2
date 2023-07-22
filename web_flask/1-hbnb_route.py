76bbe582850a37bde2406ec59713d33003017371
root <root@DESKTOP-R3J48K6>
2023-07-22 19:26:18 +0100

    Webstack

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def home():
     """Dispay Hello HBNB"""
-return "Hello HBNB!"
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
