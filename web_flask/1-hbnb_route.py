64e7bb09fdd2a51c92744fd46cc33a5c56ad137b
root <root@DESKTOP-R3J48K6>
2023-07-22 19:22:42 +0100

    web_flask

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,12 +12,14 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def home():
     """Dispay Hello HBNB"""
-    return "Hello HBNB!"
+return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Display HBNB"""
     return "HBNB"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
